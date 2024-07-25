ba994421a30268c5ad04cc8e85de48a9e1700ec0
Devansh-2512 <sr.devansh@gmail.com>
2024-07-25 12:30:21 +0530

    add function created with advanced delimeter config

--- src/App.js
+++ src/App.js
@@ -1,20 +1,75 @@
-import logo from './logo.svg';
-import './App.css';
-import { useState } from 'react';
+import { Button, Card, Input, Layout, Space } from "antd";
+import { useState } from "react";
 
 function App() {
-  const [inputString,setInputString] = useState("");
-  const [inputDelimeter,setInputDelimeter] = useState("");
-  const [resultString,setResultString] = useState("")
+  const [inputString, setInputString] = useState("");
+  const [inputDelimeter, setInputDelimeter] = useState("");
+  const [resultString, setResultString] = useState("");
 
   
+  const add = () => {
+    let newString = inputString;
+    console.log(newString, "newString");
+    if (newString.length == 0) {
+      setResultString("0");
+    } else if (inputDelimeter.length == 0) {
+      setResultString(newString);
+    } else {
+      let arrayOfNums = newString.split(inputDelimeter);
+      console.log(arrayOfNums, "arrayOfNums", inputDelimeter);
+      if (!newString.includes(inputDelimeter)) {
+        setResultString("Invalid Delimeter");
+      } else {
+        let Result = 0;
+        let negativeNumbers = [];
+        for (let nums of arrayOfNums) {
+          if (Number(nums) < 0) {
+            negativeNumbers.push(nums);
+          }
+          if (Number(nums)) {
+            Result += Number(nums);
+          }
+        }
+        if (negativeNumbers.length > 0) {
+          setResultString(
+            `Negative Numbers not allowed ${negativeNumbers.join(",")}`
+          );
+          return;
+        }
+        setResultString(Result.toString());
+      }
+    }
+  };
   return (
-    <div className="App">
-       <input placeholder='Input String of Numbers' value={inputString} onChange={(e)=>{setInputString(e.target.value)}} />
-       <input placeholder='Input Delimeter' value={inputDelimeter} onChange={(e)=>{setInputDelimeter(e.target.value)}} />
-       <button onClick={()=>{}}>Calculate</button>
-       <h2>Answer:- {resultString} </h2>
-    </div>
+    <Layout>
+      <Card>
+        <Space direction="vertical">
+          <Input.TextArea
+            placeholder="Input String of Numbers"
+            value={inputString}
+            onChange={(e) => {
+              setInputString(e.target.value);
+            }}
+          />
+          <Input
+            placeholder="Input Delimeter"
+            value={inputDelimeter}
+            onChange={(e) => {
+              setInputDelimeter(e.target.value);
+            }}
+          />
+          <Button
+            type="primary"
+            onClick={() => {
+              add();
+            }}
+          >
+            Calculate
+          </Button>
+          <h2>Result:- {resultString} </h2>
+        </Space>
+      </Card>
+    </Layout>
   );
 }
 
